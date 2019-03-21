cb393c00cb7464ffad05668260549a7b976ec053
Tu Nombre <you@example.com>
2019-03-21 14:34:01 +0100

    Comienzo de la práctica

---
+++ Date.java
@@ -0,0 +1,24 @@
+package.unileon.es.Date;
+
+
+int isSameYear();
+int isSameMonth();
+int isSameDay();
+int isSame();
+
+int returnMonth();
+int checkDay();
+ini season();
+
+int monthsLeft();
+int returnStringDate();
+int daysUntilEnd();
+int monthSameDays();
+int daysSinceFirst();
+int returnDayOfWeek();
+
+public static void main (){
+
+}
+
+
